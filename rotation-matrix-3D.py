dee3b1bc853bc4a40bc591ef47aebab20f5e6f8c
Gerard <gerardbajona@gmail.com>
2018-01-04 13:57:24 +0100

    Limit code to 80 columns

--- rotation-matrix-3D.py
+++ rotation-matrix-3D.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 # --------------------------------------------------
 # Name    : rotation-matrix-3D.py
-# Version : 1.0.1
+# Version : 1.0.2
 # Python  : 3.5.3
 # Blender : 2.78a
 # License : MIT
 # Author  : Gerard Bajona
 # Created : 29/11/2017
-# Changed : 30/11/2017
+# Changed : 04/01/2018
 # URL     : http://github.com/gerardbm/scripts
 # --------------------------------------------------
 """
@@ -22,9 +22,11 @@ pointing to one direction and the Sky Texture painting the Sky for another
 solar light direction. So this script will update the Sky Texture according to
 the Sun rotation and its solar light direction, automatically.
 
-Name of the lamp        : 'SunLamp'
-Name of the world       : 'World'
-Name of the Sky Texture : 'Sky Texture'
+Blender objects
+
+- Name of the lamp        : 'SunLamp'
+- Name of the world       : 'World'
+- Name of the Sky Texture : 'Sky Texture'
 
 Usage: rotate the Sun and run this script. Now the color of the sky gets the
 color it should have according to the Sun rotation.
@@ -40,9 +42,27 @@ zAng = bpy.data.objects['SunLamp'].rotation_euler[2]
 
 vec = mathutils.Vector((0.0, 0.0, 1.0))
 
-xMat = mathutils.Matrix(((1.0, 0.0, 0.0), (0.0, math.cos(xAng), -math.sin(xAng)), (0.0, math.sin(xAng), math.cos(xAng))))
-yMat = mathutils.Matrix(((math.cos(yAng), 0.0, math.sin(yAng)), (0.0, 1.0, 0.0), (-math.sin(yAng), 0.0, math.cos(yAng))))
-zMat = mathutils.Matrix(((math.cos(zAng), -math.sin(zAng), 0.0), (math.sin(zAng), math.cos(zAng), 0.0), (0.0, 0.0, 1.0)))
+xMat = mathutils.Matrix(
+    (
+        (1.0, 0.0, 0.0),
+        (0.0, math.cos(xAng), -math.sin(xAng)),
+        (0.0, math.sin(xAng), math.cos(xAng))
+    ),
+)
+yMat = mathutils.Matrix(
+    (
+        (math.cos(yAng), 0.0, math.sin(yAng)),
+        (0.0, 1.0, 0.0),
+        (-math.sin(yAng), 0.0, math.cos(yAng))
+    ),
+)
+zMat = mathutils.Matrix(
+    (
+        (math.cos(zAng), -math.sin(zAng), 0.0),
+        (math.sin(zAng), math.cos(zAng), 0.0),
+        (0.0, 0.0, 1.0)
+    ),
+)
 
 vec = xMat * vec
 vec = yMat * vec
